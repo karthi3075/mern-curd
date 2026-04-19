0db90c528e7d86cda903fd7f093f669c12d5ee86
karthi3075 <karthijayakumar3007@gmail.com>
2026-04-19 12:09:37 +0530

    Remove MONGO_URL log from db connection
    Remove console log for MONGO_URL before connecting to the database.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,6 @@ const mongoose=require("mongoose")
 
 const connectDB = async () => {
     try{
-        console.log(process.env.MONGO_URL)
         await mongoose.connect(process.env.MONGO_URL)
         console.log("db connected")
     }catch(error){  
